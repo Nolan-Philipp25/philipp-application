49389fbbc3bd3bc1c52edf6670bc3732d0e45a9c
Philipp Nolan <example@gmail.com>
2023-02-15 19:10:20 +0200

    left side menu

--- src/components/Layout/Header/intex.tsx
+++ src/components/Layout/Header/intex.tsx
@@ -2,39 +2,36 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import HomeIcon from '@mui/icons-material/Home';
 import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom";
 import { useGetUser} from '../../../hooks/useAuth';
 import UserMenu from './UserMenu';
 
-const Header: React.FC = () => {
-const {data} = useGetUser();
-  console.log('logged user = ', data)
+const Header = () => {
+  const {data} = useGetUser();
+
   return (
-    <AppBar component="nav" style={{position:'relative'}}>
-    <Toolbar>
-      <Typography
-        variant="h6"
-        component="div"
-        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-      >
-        <Link to="/" style={{textDecoration:'none', color:'white'}}>
-          <Button color="inherit">
-            <HomeIcon/>
-            </Button>
-        </Link>
-          
-      </Typography>
-      {
-        data && <UserMenu user={data}/>
-      }
-      {
-        !data && <Link style={{textDecoration:'none', color:'white'}} to="/login">
-          <Button color="inherit">Login</Button>
-        </Link>
-      }
-      
+    <AppBar position="relative" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <Toolbar>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+        >
+          <Link to="/" style={{textDecoration:'none', color:'white'}}>
+            Go Home
+          </Link>
+            
+        </Typography>
+        {
+          data && <UserMenu user={data}/>
+        }
+        {
+          !data && <Link style={{textDecoration:'none', color:'white'}} to="/login">
+            <Button color="inherit">Login</Button>
+          </Link>
+        }
+        
     </Toolbar>
   </AppBar>
   )
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Header from './Header/intex';
 import Box from '@mui/material/Box';
+import SideMenu from './Header/SideMenu';
+
 
 const Layout = (props:any) => {
   const { children } = props;
   return (
     <Box>
-      <Header/>
-      <Container>
-        {children}
-      </Container>
+        <Header />
+        <SideMenu />
+        <Container>
+          {children}
+        </Container>
+      
     </Box>
     
   )
