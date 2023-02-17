1399ee458c44893d3e9bc8bba39ef24182ee6b6f
Philipp Nolan <example@gmail.com>
2023-02-17 10:42:09 +0200

    add tests

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+import { render, screen } from '@testing-library/react'
+
+import App from 'src/App'
+import { useAuth, useGetUser } from 'src/hooks/useAuth'
+
+const mockedUseGetUser = useGetUser as jest.Mock<any>;
+const mockedUseAuth = useAuth as jest.Mock<any>;
+const mockedAxiosGet = axios.get as jest.Mock<any>;
+
+jest.mock("axios");
+jest.mock("../hooks/useAuth");
+
+describe("<App />", () => {
+    beforeEach(() => {
+        mockedUseGetUser.mockImplementation(() => ({ isLoading: true }));
+        mockedUseAuth.mockImplementation(() => ({ isLoading: true }));
+        mockedAxiosGet.mockImplementation(() => Promise.resolve({ data: {id: 1 }}));
+    });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("Renders without crashing", () => {
+        render(<App />);
+    });
+
+    it("Displays loading message", () => {
+        mockedUseGetUser.mockImplementation(() => ({
+            status: 'loading',
+        }));
+        render(<App />);
+        expect(screen.getByText(/You are welcome/ig)).toBeInTheDocument()
+    });
+});
